f5063f9aa2ecac3fe624b9dec806a6bc9fa892fe
Zachary51 <zaricwong51@gmail.com>
2019-02-02 14:01:31 -0800

    init

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Zachary Learn React
           </a>
         </header>
       </div>

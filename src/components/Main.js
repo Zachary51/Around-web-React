f7d6a5a6cff98bf5fa2348af0d71ab7ddd094352
Zachary51 <zaricwong51@gmail.com>
2019-02-10 16:06:30 -0800

    validate logining state

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Register } from "./Register";
 import { Login } from "./Login";
+import { Home } from "./Home";
 import { Switch, Route } from 'react-router-dom';
 
 export class Main extends React.Component {
+  getLogin = () => {
+    return this.props.isLoggedIn ? <Home/> :
+        <Login handleSuccessfulLogin={this.props.handleSuccessfulLogin}/>;
+  }
+
   render() {
     return (
         <div className="main">
           <Switch>
-            <Route exact path="/" component={Login}/>
-            <Route path="/login" component={Login}/>
+            <Route exact path="/" render={this.getLogin}/>
+            <Route path="/login" render={this.getLogin}/>
             <Route path="/register" component={Register}/>
-            <Route component={Login}/>
+            <Route path="/home" component={Home}/>
+            <Route render={this.getLogin}/>
           </Switch>
         </div>
     );
